5d6cea76f6eff58adf98d45b2d3d4811662773bf
DeepaLaunchCode <sonaliforamit@gmail.com>
2025-06-18 14:45:33 -0500

    updated the code for album movie and books to use sql

--- src/main/java/com/example/springBoot2/models/Album.java
+++ src/main/java/com/example/springBoot2/models/Album.java
@@ -1,6 +1,9 @@
 package com.example.springBoot2.models;
-
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
 public class Album {
     private String name;
     private int year;
@@ -9,12 +12,17 @@ public class Album {
 
     public Album() {
     }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-    public Album(String name, String artist, int year, int tracks) {
+
+    public Album(String name, String artist, int year, int tracks, int id) {
         this.name = name;
         this.year = year;
         this.artist = artist;
         this.tracks = tracks;
+        this.id=id;
     }
 
     public String getName() {
@@ -48,4 +56,13 @@ public class Album {
     public void setTracks(int tracks) {
         this.tracks = tracks;
     }
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
--- src/main/java/com/example/springBoot2/models/Movie.java
+++ src/main/java/com/example/springBoot2/models/Movie.java
@@ -1,5 +1,9 @@
 package com.example.springBoot2.models;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
 public class Movie  {
     private String name;
     private int year;
@@ -8,12 +12,16 @@ public class Movie  {
 
     public Movie() {
     }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-    public Movie(String name, String director, int year, int duration) {
+    public Movie(String name, String director, int year, int duration,int id) {
         this.name = name;
         this.year = year;
         this.director = director;
         this.runtime = duration;
+        this.id=id;
     }
 
     public String getName() {
@@ -47,4 +55,12 @@ public class Movie  {
     public void setRuntime(int runtime) {
         this.runtime = runtime;
     }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
