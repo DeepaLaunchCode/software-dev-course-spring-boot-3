4966a9a62def2d22d7495b6ce914fa70d54a38bc
DeepaLaunchCode <sonaliforamit@gmail.com>
2025-06-18 15:39:26 -0500

    ouput screenshot added

---
+++ src/main/java/com/example/springBoot2/repositories/AlbumRepository.java
@@ -0,0 +1,6 @@
+package com.example.springBoot2.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.springBoot2.models.Album;
+
+public interface AlbumRepository extends JpaRepository<Album, Integer> {
+}
